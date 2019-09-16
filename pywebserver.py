e6dbb5cb6b72ad5f1d5d1571e6512122aa185cfd
545u <47539026+545u@users.noreply.github.com>
2019-09-16 09:09:59 +0300

    This run the server.

--- pywebserver.py
+++ pywebserver.py
@@ -3,13 +3,13 @@ import socketserver
 
 print("#####################")
 print("#    pywebserver    #")
-print("# Default port: 80  #")
-print("#      By: 545u     #")
+print("#     Port: 80      #")
+print("#     By: 545u      #")
 print("#####################")
 
 PORT = 80
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("running at port", PORT)
+    print("Server is running at port", PORT)
     httpd.serve_forever()
